0fe5662ecf2c8d5688b065aeecb28188ebc77d85
raiparag123 <rai.parag123@gmail.com>
2021-08-12 12:33:49 +0530

    p

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php 
+
+session_start();
+
+echo $_SESSION["db_connect"];
+
+// echo 'Server name: '.$_SERVER['SERVER_NAME'].'<br>';
+
+?>
+
+
+<a href="index.php"> HELLO </a>
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php 
 
-echo "Hello World";
+session_start();
 
-echo 'Server name: '.$_SERVER['SERVER_NAME'].'<br>';
+if($_SERVER['SERVER_NAME'] == "localhost")
+
+    $_SESSION["db_connect"] ="db_localhost.php";
+else
+    $_SESSION["db_connect"] ="db.php";
+
+    echo $_SESSION["db_connect"];
+
+
+// echo 'Server name: '.$_SERVER['SERVER_NAME'].'<br>';
 
 ?>
+
+
+<a href="header.php"> HELLO </a>
